6be834c69f8974bc5b497adfe8a9805d0e24729b
Viole Hours <holasoyviole@gmail.com>
2024-11-22 14:06:49 -0300

    Set up the base of the home page, a background component, and Helmet used in home

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,13 +5,14 @@ import {
 } from "react-router-dom";
 import App from "./App";
 import NotFound from "./pages/NotFound/NotFound";
+import Home from "./pages/Home/Home";
+
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <>
-            <Route path="/" element={<App />}>
-                <Route path="/home" element={<App />} />
-            </Route>
+            <Route path="/" element={<App />} />
+            <Route path="/home" element={<Home />} />
             {/* Ruta 404 */}
             <Route path="*" element={<NotFound />} />
         </>
