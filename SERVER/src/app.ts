a98b87addaac721d0fe98d0b861a5de7a1cb6bd6
Luis Romero <lueduar15@gmail.com>
2024-11-24 16:57:07 -0400

    Fix: Adjust Sequelize configuration for local and production environments

--- SERVER/src/app.ts
+++ SERVER/src/app.ts
@@ -31,7 +31,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       secure: process.env.NODE_ENV === "production",
-      maxAge: 24 * 60 * 60 * 1000, // 1 día
+      maxAge: 24 * 60 * 60 * 1000,
     },
   })
 );
--- SERVER/src/config/connection.ts
+++ SERVER/src/config/connection.ts
@@ -1,7 +1,27 @@
-import { Sequelize } from "sequelize";
-import { envs } from "./envs";
+// import { Sequelize } from "sequelize";
+// import { envs } from "./envs";
 
-export const sequelize = new Sequelize(envs.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-});
+// export const sequelize = new Sequelize(envs.DATABASE_URL, {
+//   dialect: "postgres",
+//   logging: false,
+// });
+
+import { Sequelize } from "sequelize";
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const sequelize = new Sequelize(
+  process.env.DATABASE_URL as string,
+  {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+  }
+);
