7e2740b9feafe9eef0ccb1d6d25f531e0425e4af
Mateo Scienza <mateoscns@gmail.com>
2024-11-28 22:48:48 -0300

    fixes

--- SERVER/src/index.ts
+++ SERVER/src/index.ts
@@ -7,14 +7,40 @@ const PORT = envs.PORT || 3000;
 
 const main = async () => {
   try {
+  
     await sequelize.sync();
-    console.log("DB connected");
-    app.listen(PORT);
+    console.log("Database connected successfully");
+
+   
+    const server = app.listen(PORT);
     console.log(`Server running on port ${PORT}`);
-    await sequelize.sync();
+
+   
+    process.on('SIGTERM', async () => {
+      console.log('Received SIGTERM. Performing graceful shutdown...');
+      await sequelize.close();
+      server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+      });
+    });
+
+    process.on('SIGINT', async () => {
+      console.log('Received SIGINT. Performing graceful shutdown...');
+      await sequelize.close();
+      server.close(() => {
+        console.log('Server closed');
+        process.exit(0);
+      });
+    });
+
   } catch (error) {
-    console.log(error);
+    console.error('Error during startup:', error);
+    process.exit(1);
   }
 };
 
-main();
+main().catch(error => {
+  console.error('Unhandled error:', error);
+  process.exit(1);
+});
--- SERVER/src/models/initModels.ts
+++ SERVER/src/models/initModels.ts
@@ -1,7 +1,37 @@
 import { sequelize } from "@config/connection";
 import { User } from "./User";
 import { OTP } from "./OTP";
+import { Account } from "./account";
 
-// Inicializar modelos
-User.initModel(sequelize);
-OTP.initModel(sequelize);
+export const initModels = () => {
+
+  User.initModel(sequelize);
+  OTP.initModel(sequelize);
+  Account.initModel(sequelize);
+
+ 
+  Account.belongsTo(User, { 
+    foreignKey: 'userId',
+    as: 'user'
+  });
+  
+  User.hasMany(Account, { 
+    foreignKey: 'userId',
+    as: 'accounts' 
+  });
+
+  OTP.belongsTo(User, {
+    foreignKey: 'userEmail',
+    targetKey: 'email',
+    as: 'user'
+  });
+
+  User.hasMany(OTP, {
+    foreignKey: 'userEmail',
+    sourceKey: 'email', 
+    as: 'otps'
+  });
+};
+
+
+initModels();
